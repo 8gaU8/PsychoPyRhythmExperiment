8a2d24cae3d6f1c20b1da250ff0e05021a1519b0
8gaU8 <haga.yuya2.2016@gmail.com>
2023-08-02 14:07:32 +0900

    codeモジュールのテストの追加

---
+++ run_stim_test.py
@@ -0,0 +1,22 @@
+from serial import Serial
+from rhythm_trial_code.dummy_serial import Serial
+
+from rhythm_trial_code.main import run_stim
+
+
+def main():
+    port = Serial("COM03")
+    port.write([0])
+    try:
+        run_stim(port, 0.2, scale=0.8, stim_sound_file="./sound/SD0050.WAV", sound=True)
+    finally:
+        port.write([0])
+        port.close()
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+
+    os.chdir(Path(__file__).parent)
+    main()
