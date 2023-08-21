5ad48ed03281efe264cd67253ca3db6fe4c4bf50
8gaU8 <haga.yuya2.2016@gmail.com>
2023-08-21 12:41:31 +0900

    メタトリガーのテスト

--- run_stim_test.py
+++ run_stim_test.py
@@ -1,17 +1,22 @@
-from serial import Serial
+from time import sleep
 
-from rhythm_trial_code.dummy_serial import Serial
 from rhythm_trial_code.main import run_stim
+from rhythm_trial_code.message import play_trigger_factory
+from rhythm_trial_code.serial_trigger import init_port
 
 
 def main():
-    port = Serial("COM03")
-    port.write([0])
+    port = init_port("COM03", dummy=True)
+    _play_trigger_meta = play_trigger_factory(port, [4])
+    play_trigger_meta = lambda: _play_trigger_meta(True)
+    play_trigger_meta()
+
     try:
-        run_stim(port, 0.2, scale=0.8, stim_sound_file="./sound/SD0050.WAV", sound=True)
+        run_stim(port, 0.2, scale=1, stim_sound_file="./sound/SD0050.WAV", sound=True)
     finally:
         port.write([0])
         port.close()
+    sleep(1)
 
 
 if __name__ == "__main__":
