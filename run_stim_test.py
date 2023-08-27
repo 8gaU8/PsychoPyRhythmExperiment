bf5378533501a44b18c52937e7347d9bead5c64d
8gaU8 <haga.yuya2.2016@gmail.com>
2023-08-27 11:44:03 +0900

    rhythm_trial_codeに対応

--- run_stim_test.py
+++ run_stim_test.py
@@ -1,18 +1,25 @@
 from time import sleep
 
 from rhythm_trial_code.main import run_stim
-from rhythm_trial_code.message import play_trigger_factory
 from rhythm_trial_code.serial_trigger import init_port
+from rhythm_trial_code.message import PlayFactories as _PF
 
 
 def main():
     port = init_port("COM03", dummy=True)
-    _play_trigger_meta = play_trigger_factory(port, [4])
+    _play_trigger_meta = _PF([]).trig_fctr(port, [4])
     play_trigger_meta = lambda: _play_trigger_meta(True)
+
     play_trigger_meta()
 
     try:
-        run_stim(port, 0.2, scale=1, stim_sound_file="./sound/SD0050.WAV", sound=True)
+        run_stim(
+            port=port,
+            delay=-0.5,
+            scale=0.8,
+            soundfiles=["./sound/SD1010.WAV", "./sound/SD0050.WAV"],
+            sound=True,
+        )
     finally:
         port.write([0])
         port.close()
