9d090b6842c08f4b5e55631497828c52520c1e3d
8gaU8 <haga.yuya2.2016@gmail.com>
2023-09-04 11:01:13 +0900

    1拍目を強拍にアップデート

--- run_stim_test.py
+++ run_stim_test.py
@@ -1,18 +1,37 @@
 from time import sleep
 
 from rhythm_trial_code.main import run_stim
-from rhythm_trial_code.message import play_trigger_factory
+from rhythm_trial_code.message import PlayFactories as _PF
 from rhythm_trial_code.serial_trigger import init_port
 
 
 def main():
     port = init_port("COM03", dummy=True)
-    _play_trigger_meta = play_trigger_factory(port, [4])
+    _play_trigger_meta = _PF([]).trig_fctr(port, [4])
     play_trigger_meta = lambda: _play_trigger_meta(True)
     play_trigger_meta()
 
     try:
-        run_stim(port, 0.2, scale=1, stim_sound_file="./sound/SD0050.WAV", sound=True)
+        delay = -0.152690
+        delay = -0.2
+        scale = 1.2
+        run_stim(
+            port,
+            delay=delay,
+            scale=scale,
+            soundfiles=["./sound/SD1010.WAV", "./sound/SD0050.WAV"],
+            sound=True,
+        )
+        sleep(1.0)
+        scale = 1
+        delay = 0
+        run_stim(
+            port,
+            delay=delay,
+            scale=scale,
+            soundfiles=["./sound/SD1010.WAV", "./sound/SD0050.WAV"],
+            sound=True,
+        )
     finally:
         port.write([0])
         port.close()
