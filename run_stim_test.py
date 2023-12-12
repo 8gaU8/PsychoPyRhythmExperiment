c3110cdcd0cc85041c1fb4d3987de15bdcd4d6c1
8gaU8 <haga.yuya2.2016@gmail.com>
2023-12-12 12:20:56 +0900

    テストコード更新

--- run_stim_test.py
+++ run_stim_test.py
@@ -7,7 +7,7 @@ from rhythm_trial_code.serial_trigger import init_port
 
 
 def main():
-    port = init_port("COM03", dummy=True)
+    port = init_port("COM3", dummy=False)
     _play_trigger_meta = _PF([]).trig_fctr(port, [4])
     play_trigger_meta = lambda: _play_trigger_meta(True)
     play_trigger_meta()
@@ -27,15 +27,6 @@ def main():
         play_stim(stim_series=stim, sound=True)
         print(time.time() - t0)
         sleep(1.0)
-        # scale = 1
-        # delay = 0
-        # run_stim(
-        #     port,
-        #     delay=delay,
-        #     scale=scale,
-        #     soundfiles=["./sound/SD1010.WAV", "./sound/SD0050.WAV"],
-        #     sound=True,
-        # )
     finally:
         port.write([0])
         port.close()
